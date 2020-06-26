75669f443d220393d05a6bf1eab65f669dd869bf
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2020-06-26 17:01:28 -0500

    Add reusable blocks tab to inserter.

--- packages/e2e-tests/specs/editor/various/reusable-blocks.test.js
+++ packages/e2e-tests/specs/editor/various/reusable-blocks.test.js
@@ -3,6 +3,7 @@
  */
 import {
 	insertBlock,
+	insertReusableBlock,
 	createNewPost,
 	clickBlockToolbarButton,
 	pressKeyWithModifier,
@@ -114,7 +115,7 @@ describe( 'Reusable blocks', () => {
 
 	it( 'can be inserted and edited', async () => {
 		// Insert the reusable block we created above
-		await insertBlock( 'Greeting block' );
+		await insertReusableBlock( 'Greeting block' );
 
 		// Put the reusable block in edit mode
 		const editButton = await page.waitForXPath(
@@ -198,7 +199,7 @@ describe( 'Reusable blocks', () => {
 
 		// Step 3. Insert the block created in Step 1.
 
-		await insertBlock( 'Awesome block' );
+		await insertReusableBlock( 'Awesome block' );
 
 		// Check that we have a reusable block on the page
 		const block = await page.$(
@@ -216,7 +217,7 @@ describe( 'Reusable blocks', () => {
 
 	it( 'can be converted to a regular block', async () => {
 		// Insert the reusable block we edited above
-		await insertBlock( 'Surprised greeting block' );
+		await insertReusableBlock( 'Surprised greeting block' );
 
 		// Convert block to a regular block
 		await clickBlockToolbarButton( 'More options' );
@@ -241,7 +242,7 @@ describe( 'Reusable blocks', () => {
 
 	it( 'can be deleted', async () => {
 		// Insert the reusable block we edited above
-		await insertBlock( 'Surprised greeting block' );
+		await insertReusableBlock( 'Surprised greeting block' );
 
 		// Delete the block and accept the confirmation dialog
 		await clickBlockToolbarButton( 'More options' );
@@ -322,7 +323,7 @@ describe( 'Reusable blocks', () => {
 
 	it( 'multi-selection reusable block can be converted back to regular blocks', async () => {
 		// Insert the reusable block we edited above
-		await insertBlock( 'Multi-selection reusable block' );
+		await insertReusableBlock( 'Multi-selection reusable block' );
 
 		// Convert block to a regular block
 		await clickBlockToolbarButton( 'More options' );
